d9f2224bfc33e2a764d943d805bbae4650f1bac2
Saqib Tariq <mister_saqib@rocketmail.com>
2019-03-05 21:00:58 +0500

    image and date work

--- 28-02-2019/Models/User.php
+++ 28-02-2019/Models/User.php
@@ -1,8 +1,7 @@
 <?php
 
+class User {
 
-class User{
-    
     private $user_name;
     private $name;
     private $email;
@@ -10,42 +9,101 @@ class User{
     private $password;
     private $gender;
     private $profile_image;
-    
-    public function __set($name, $value) {  
+
+    public function __set($name, $value) {
         $methodName = "set";
         $explode = explode("_", $name);
         $ucfirst = array_map('ucfirst', $explode);
         $implode = implode("", $ucfirst);
         $methodName .= $implode;
-        if(!method_exists($this, $methodName)){
+        if (!method_exists($this, $methodName)) {
             throw new Exception("Setter Function not exists : " . $name);
         }
         $this->$methodName($value);
     }
-    
+
     public function __get($name) {
         ;
     }
-    
-    private function setUserName($value){
-        if(empty($value)){
+
+    private function setUserName($value) {
+//        $value .= "     ";
+//        ltrim($str)
+//        rtrim($str)
+        $value = trim($value);
+        if (empty($value)) {
             throw new Exception("User Name is Empty");
         }
         $this->user_name = $value;
-        dd($value);
     }
-    
-    private function setName(){
-        
-    }
-    
-    private function setEmail(){
-        
-    }
-    
-    
-}
 
+    private function setName($value) {
+        $value = trim($value);
+        if (empty($value)) {
+            throw new Exception("Name is Empty");
+        }
+        $this->name = $value;
+    }
+
+    private function setEmail($value) {
+        // regax pending
+
+        $value = trim($value);
+        if (empty($value)) {
+            throw new Exception("Email requried");
+        }
+        $this->email = $value;
+    }
+
+    private function setDob($value) {
+        extract($value);
+//        dd($year);
+        if (!checkdate($month, $day, $year)) {
+            throw new Exception("Date is invalid");
+        }
+//        dd(date());
+//        dd((1551799884 + 3600) - time());
+
+        $dob = mktime(0, 0, 0, $month, $day, $year);
+
+        $this->dob = $dob;
+    }
+
+    private function setPassword($value) {
+
+        $value = trim($value);
+        if (empty($value)) {
+            throw new Exception("password is Empty");
+        }
+
+        if (strlen($value) < 5) {
+            throw new Exception("Your password less then 5");
+        }
+
+        $this->password = sha1($value);
+    }
+
+    private function setProfileImage($image) {
+
+        if ($image['error'] == 4) {
+            throw new Exception('Please select the image');
+        }
+
+        if ($image['type'] !== "image/jpeg") {
+            throw new Exception('Please select JPEG image');
+        }
+        
+        
+        $image_size = getimagesize($image['tmp_name']);
+        
+        
+        if($image['type'] !== $image_size['mime']){
+             throw new Exception('Image not valid');
+        }
+        dd($image_size);
+    }
+
+}
 ?>
 
 
--- 28-02-2019/registration.php
+++ 28-02-2019/registration.php
@@ -6,55 +6,106 @@ function dd($arg) {
     print_r($arg);
     die;
 }
+function dd1($arg) {
+    echo "<pre>";
+    var_dump($arg);
+    die;
+}
+
 $errors = array();
 
 if ($_POST) {
-
+//dd($_POST);
 //    isset()
 //    empty()
 
-if(class_exists('User')){
-    $user = new User();
-}
+    if (class_exists('User')) {
+        $user = new User();
+    }
 
 
-    try{
-        $user->user_name = !empty($_POST['uname']) ? $_POST['uname'] : '';
+    try {
+        $user->user_name = !empty($_POST['user_name']) ? $_POST['user_name'] : '';
     } catch (Exception $ex) {
+        $errors['user_name'] = $ex->getMessage();
+    }
+    try {
+        $user->name = !empty($_POST['name']) ? $_POST['name'] : '';
+    } catch (Exception $ex) {
+        $errors['name'] = $ex->getMessage();
+    }
+    try {
+        $user->email = !empty($_POST['email']) ? $_POST['email'] : '';
+    } catch (Exception $ex) {
+        $errors['email'] = $ex->getMessage();
+    }
+    try {
+        $dateOfBirth = array();
+        $dateOfBirth['day'] = !empty($_POST['day']) ? (int) $_POST['day'] : '';
+        $dateOfBirth['month'] = !empty($_POST['month']) ? (int) $_POST['month'] : '';
+//        $dateOfBirth['month'] = 2;
+        $dateOfBirth['year'] = !empty($_POST['year']) ? (int) $_POST['year'] : '';;
+        $user->dob = $dateOfBirth;
+        
+    } catch (Exception $ex) {
+        $errors['dob'] = $ex->getMessage();
+    }
 
+
+    try {
+        $user->password = !empty($_POST['password']) ? $_POST['password'] : '';
+    } catch (Exception $ex) {
+        $errors['password'] = $ex->getMessage();
+    }
+
+    // retype passowrd
+
+    try {
+        $user->gender = !empty($_POST['gender']) ? $_POST['gender'] : '';
+    } catch (Exception $ex) {
+        $errors['gender'] = $ex->getMessage();
+    }
+    try {
+        $user->profile_image = !empty($_FILES['image']) ? $_FILES['image'] : '';
+    } catch (Exception $ex) {
+        $errors['image'] = $ex->getMessage();
     }
     
-    $user->user_name = !empty($_POST['uname']) ? $_POST['uname'] : '';
-    
-    $user->user_name = !empty($_POST['uname']) ? $_POST['uname'] : '';
-}
+    dd($errors);
+} 
 
-//dd/();
 ?>
+<style>
+    .registration{
+        max-width: 500px;
+        width: 100%;
+        margin: 0 auto;   
+    }
+</style>
 
-
-<form method="post" action="">
-    <input type="text" name="uname" placeholder="User Name"><br>
+<form method="post" action="" class="registration" enctype="multipart/form-data">
+    <input type="text" name="user_name" placeholder="User Name"><br>
     <input type="text" name="name" placeholder="Enter Your name"><br>
     <input type="email" name="email" placeholder="Email">
     <br>
     <select name="day">
         <option value="1">1</option>
-        <option value="1">1</option>
-        <option value="1">1</option>
-        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="31">31</option>
     </select>
     <select name="month">
         <option value="1">1</option>
-        <option value="1">1</option>
-        <option value="1">1</option>
-        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
     </select>
     <select name="year">
-        <option value="1">1</option>
-        <option value="1">1</option>
-        <option value="1">1</option>
-        <option value="1">1</option>
+        <option value="2010">2010</option>
+        <option value="2010">2010</option>
+        <option value="2010">2010</option>
+        <option value="2010">2010</option>
     </select>
     <br>
     <input type="password" name="password" placeholder="password">
